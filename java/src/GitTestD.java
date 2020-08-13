f608c69b1496f36a6ee09ac4d27667ff1787beb4
luyujie <luyj@dqist.com>
2020-08-13 10:41:03 +0800

    解决GitTestD的冲突

--- java/src/GitTestD.java
+++ java/src/GitTestD.java
@@ -2,5 +2,6 @@ public class GitTestD {
     public static void main(String[] args) {
         System.out.println("GitTestD");
         System.out.println("我要吃KFC");
+        System.out.println("我要吃汉堡王");
     }
 }
